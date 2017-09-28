cca48d2bda51e3b35d7ba099372bb1e7992adb47
Nghia Dinh <nghia.dinh@s3corp.com.vn>
2017-09-28 18:27:49 +0700

    add source code

---
+++ NDYoutubePlayer/NDYoutubePlayer/NDYoutubePlayer.swift
@@ -0,0 +1,69 @@
+//
+//  NDYoutubePlayer.swift
+//  NDYoutubeKit
+//
+//  Created by user on 9/28/17.
+//  Copyright © 2017 Den. All rights reserved.
+//
+
+import Foundation
+import AVKit
+
+class NDYoutubePlayer: AVPlayerLayer {
+    var youtubePlayer: AVPlayer!
+    override init() {
+        super.init()
+    }
+    
+    func playVideo(indentifier: String, quality: NDYouTubeVideoQuality? = nil, isAutoPlay: Bool = true) {
+        if youtubePlayer != nil {
+            youtubePlayer.pause()
+            youtubePlayer.replaceCurrentItem(with: nil)
+            youtubePlayer = nil
+        }
+        NDYoutubeClient.shared.getVideoWithIdentifier(videoIdentifier: indentifier) { [weak self] (video, err) in
+            guard let video = video else { return }
+            guard let streamURLs = video.streamURLs else { return }
+            if let quality = quality {
+                if let videoString = streamURLs[quality.rawValue] {
+                    self?.youtubePlayer = AVPlayer(url: URL(string: videoString as! String)!)
+                    self?.player = self?.youtubePlayer
+                    if isAutoPlay {
+                        self?.youtubePlayer.play()
+                    }
+                }
+            }
+            else if let videoString = streamURLs[NDYouTubeVideoQuality.NDYouTubeVideoQualityHD720.rawValue] ?? streamURLs[NDYouTubeVideoQuality.NDYouTubeVideoQualityMedium360.rawValue] ?? streamURLs[NDYouTubeVideoQuality.NDYouTubeVideoQualitySmall240.rawValue] {
+                self?.youtubePlayer = AVPlayer(url: URL(string: videoString as! String)!)
+                self?.player = self?.youtubePlayer
+                if isAutoPlay {
+                    self?.youtubePlayer.play()
+                }
+            }
+        }
+    }    
+    
+    func stop() {
+        if youtubePlayer != nil {
+            youtubePlayer.pause()
+            youtubePlayer.replaceCurrentItem(with: nil)
+            youtubePlayer = nil
+        }
+    }
+    
+    func play() {
+        if youtubePlayer != nil {
+            youtubePlayer.play()
+        }
+    }
+    
+    func pause() {
+        if youtubePlayer != nil {
+            youtubePlayer.pause()
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ NDYoutubePlayer/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  NDYoutubePlayer
+//
+//  Created by user on 9/28/17.
+//  Copyright © 2017 Den. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var videoLayerView: UIView!
+    @IBOutlet weak var indentifierTextField: UITextField!
+    var youtubeVideoPlayer: NDYoutubePlayer!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setUpYotubeVideoPlayer()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func setUpYotubeVideoPlayer() {
+        youtubeVideoPlayer = NDYoutubePlayer()
+        self.videoLayerView.layer.addSublayer(youtubeVideoPlayer)
+        youtubeVideoPlayer.frame = self.videoLayerView.frame
+    }
+    func playVideo(indentifier: String) {
+        youtubeVideoPlayer.playVideo(indentifier: indentifier)
+    }
+    
+    @IBAction func playButtonPressed(sender: UIButton) {
+        if indentifierTextField.text!.isEmpty {
+            return
+        }
+        self.playVideo(indentifier: indentifierTextField.text!)
+    }
+
+
+
+}
+
